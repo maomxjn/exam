1d34cea81ec4c94a41e5b78d5c67f7b44064e7da
Michelle <146042478+maomxjn@users.noreply.github.com>
2023-12-12 09:57:22 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+
+        public class myClass<T> where T : struct
+        {
+            private List<T> list = new List<T>();
+
+                public void AddList(T item)
+            {
+                list.Add(item);
+            }
+            public List<T> GetLi() 
+            {
+                list.ToList();
+            }
+            public List<T> SortList()
+            {
+                list.Sort().Tolist();
+            }
+
+        }
+
+    }
+}
